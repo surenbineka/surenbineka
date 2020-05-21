017bcd06edc0808cff6bf1151866fd38baa579b4
Sashitha Chamara <sashithachamara@live.com>
2020-05-21 15:46:10 +0530

    single one line quick running/updating

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-docker rm cloud-torrent cloud-torrent-files 
+docker stop cloud-torrent cloud-torrent-files 
+docker rm cloud-torrent cloud-torrent-files
+docker system prune -a
+rm -r torrentfast.net
+git clone https://github.com/sashithacj/torrentfast.net.git
+cd torrentfast.net
 docker-compose up
